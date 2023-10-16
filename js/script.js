da74e3ff1b9b549fc48229e316e6de57482231ad
Victor Dominguez <vdomim@gmail.com>
2023-10-17 00:48:46 +0200

    Adding the password validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const pass = document.querySelector('#pass')
 const passConfirm = document.querySelector('#pass_confirm')
+const validator = document.querySelector('.validator')
+const submit = document.querySelector('.submit')
+const form = document.querySelector('form')
+let passCheck = false
+
 passConfirm.addEventListener('input', () => {
     if(pass.value !== passConfirm.value){
-        console.log("no coincide");
+        passConfirm.style.border = '2px solid red';
+        passCheck=false
+        passConfirm.validationMessa
+        passConfirm.setCustomValidity("Las contraseñas deben de coincidir")
+    }
+    else{
+        passConfirm.style.border = '';
+        passCheck=true
+        passConfirm.setCustomValidity("")
     }
 })
-console.log("hola");
+
+validator.addEventListener('click', (e) => {
+    e.preventDefault()
+    submit.click()
+})
